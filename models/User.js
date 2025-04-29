2801bd00b33fd14c3a37c5bf09ef68507f75dfc2
HoaCoLau <thuanntph51789@gmail.com>
2025-04-29 16:25:43 +0700

    .

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const sequelize = require('../config/db');
 const User = sequelize.define('User', {
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   name: { type: DataTypes.STRING, allowNull: false },
+  image: { type: DataTypes.STRING, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false }
 }, {
   tableName: 'users',
